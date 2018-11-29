3cc0c586841bd0b6fc82cb3aea769022e1cff35c
wafistos <mameri.wafi@gmail.com>
2018-11-29 18:51:56 +0100

    Ajout d'un fichier python1 pour le tuto dico et fichier

---
+++ python1.py
@@ -0,0 +1,13 @@
+#Suite du cours de Jason sur les dictionnaire 
+
+mon_dictionnaire = {}#Creation d'un dico vide 
+mon_dictionnaire1= {
+    "nom": "Wafi",
+    "prenom": "mameri",
+    "age": 43
+}
+
+
+print(mon_dictionnaire1)
+
+
