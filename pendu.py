22f2a8111c594bbb0003ae2e382280dc9104d7f1
wafistos <mameri.wafi@gmail.com>
2018-11-30 16:23:00 +0100

    Add fichier pendu

--- pendu.py
+++ pendu.py
@@ -1,3 +1,14 @@
 """ Tuto de openclassroom LE PENDUE  
 """
+from donnes import *
+from fonctions import *
 
+
+
+liste = {"wafi":"homme", "meriem":"femme", "jimmy":"garcon"}
+
+for element  in liste:
+    if element == "wafi":
+        print("le joueur que vous chercher est existant")
+    else:
+        print("nothing..")
