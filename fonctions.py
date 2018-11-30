5ac1c40fc20a9a0ccc032450720edb61775b32e9
wafistos <mameri.wafi@gmail.com>
2018-11-30 23:26:05 +0100

    Mise a jours avant de dormir

--- fonctions.py
+++ fonctions.py
@@ -1,9 +1,29 @@
 """Fichier qui contient les fonctions """
 dico={}
 
-mon_fichier = open("fichier.txt", "r")
-contenu = mon_fichier.readlines()
-print(type(contenu))
+#Creer une fonction qui verifie si le nom du joueur existe ou non 
+def verifier(nom_joueur):
+    with open("fichier.txt", "r") as mon_fichier:
+        contenu1 = {} 
+        contenu = mon_fichier.read()
+        contenu = contenu.splitlines()
+    for ele in contenu:
+         
+        contenu1 =  ele.split(":")
+
+
+    print(contenu1)
+    for element in contenu:
+        if element == nom_joueur:
+            print("bonjour {} ".format(nom_joueur))
+            return True
+        else:
+            print("Bienvenue vous etes un nouvelle joueur")
+            return False
+
+verifier("wafistos")
+
+
 #print(contenu)
 
     
